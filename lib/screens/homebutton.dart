a28cefe7f7e9fb88d1afa9c6cd93cd414387f5f5
srikanthsarma145 <srikanthsarma145@gmail.com>
2020-05-27 16:37:06 +0530

    commit to 'master'

--- lib/screens/homebutton.dart
+++ lib/screens/homebutton.dart
@@ -7,7 +7,7 @@ import 'package:metropay/utilities/loading.dart';
 import 'package:toast/toast.dart';
 import 'dart:math';
 import 'package:provider/provider.dart';
-//import 'package:metropay/models/user.dart';
+import 'package:metropay/models/user.dart';
 
 class HomeButton extends StatefulWidget {
   @override
@@ -29,78 +29,55 @@ class _HomeButtonState extends State<HomeButton> {
   String destination;
   String fare;
 
+
   String boardingPoint = 'Boarding Point';
   String destinationPoint = 'Destination Point';
 
   Widget _boardingPointBtn() {
-    final user = Provider.of<User>(context);
-
-    return StreamBuilder<UserData>(
-      stream: DatabaseService(uid: user.uid).userData,
-      builder: (context, snapshot) {
-        if(snapshot.hasData){
-          UserData userData = snapshot.data;
-          _currentName = userData.name;
-          _currentbalance = userData.balance;
-          _currentdestination = userData.destination;
-          _currentboarding = userData.boarding;
-
-          return Container(
-            padding: EdgeInsets.symmetric(vertical: 25.0),
-            width: double.infinity,
-            child: RaisedButton(
-              elevation: 5.0,
-              onPressed: (){
-                if(boardingPoint=='Boarding Point'){
-                  FlutterNfcReader.read().then((response) {
-                    print(response.content);
-                  });
-                  boarding = rnd.nextInt(5).toString();
-                  while(double.parse(boarding)==0){
-                    boarding = rnd.nextInt(5).toString();
-                  }
-                  _currentboarding=boarding;
-                  boardingPoint = 'Station ' +boarding;
-                  DatabaseService(uid: user.uid).updateUserData(
-                      _currentName ?? snapshot.data.name,
-                      _currentbalance ?? snapshot.data.balance,
-                      _currentboarding ?? snapshot.data.boarding,
-                      _currentdestination ?? snapshot.data.destination
-                  );
-                }
-                else{
-                  return Loading();
-                }
-                setState(() {});
-              },
-              padding: EdgeInsets.all(15.0),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5.0),
-              ),
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 25.0),
+      width: double.infinity,
+      child: RaisedButton(
+        elevation: 5.0,
+        onPressed: (){
+          if(boardingPoint=='Boarding Point'){
+            FlutterNfcReader.read().then((response) {
+              print(response.content);
+            });
+            boarding = rnd.nextInt(5).toString();
+            while(double.parse(boarding)==0){
+              boarding = rnd.nextInt(5).toString();
+            }
+            boardingPoint = 'Station ' +boarding;
+          }
+          else{
+            return null;
+          }
+          setState(() {});
+        },
+        padding: EdgeInsets.all(15.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5.0),
+        ),
 //        color: Colors.white,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Icon(Icons.location_on,
-                    color: Colors.red,),
-                  Text(
-                    boardingPoint,
-                    style: TextStyle(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Icon(Icons.location_on,
+              color: Colors.red,),
+            Text(
+              boardingPoint,
+              style: TextStyle(
 //            color: Color(0xFF478DE0),
-                      letterSpacing: 1.5,
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'OpenSans',
-                    ),
-                  ),
-                ],
+                letterSpacing: 1.5,
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'OpenSans',
               ),
             ),
-          );
-        }else{
-          return Loading();
-        }
-      }
+          ],
+        ),
+      ),
     );
   }
 
@@ -109,66 +86,99 @@ class _HomeButtonState extends State<HomeButton> {
     final user = Provider.of<User>(context);
 
     return StreamBuilder<UserData>(
-      stream: DatabaseService(uid: user.uid).userData,
-      builder: (context, snapshot) {
-        if(snapshot.hasData){
-          UserData userData = snapshot.data;
-          _currentName = userData.name;
-          _currentbalance = userData.balance;
-          _currentdestination = userData.destination;
-          _currentboarding = userData.boarding;
+        stream: DatabaseService(uid: user.uid).userData,
+        builder: (context, snapshot) {
+          if(snapshot.hasData){
+            UserData userData = snapshot.data;
+            _currentName = userData.name;
+            _currentbalance = userData.balance;
+            _currentdestination = userData.destination;
+            _currentboarding = userData.boarding;
 
-          return  Container(
-            padding: EdgeInsets.symmetric(vertical: 25.0),
-            width: double.infinity,
-            child: RaisedButton(
-              elevation: 5.0,
-              onPressed: (){
-                if(boardingPoint != 'Boarding Point' && destinationPoint=='Destination Point'){
-                  FlutterNfcReader.read().then((response) {
-                    Toast.show("Working!!!", context, duration: Toast.LENGTH_LONG, gravity:  Toast.BOTTOM);
-                    print(response.content);
-                  });
-                  destination = rnd.nextInt(5).toString();
-                  while(double.parse(destination)==0){
+            return  Container(
+              padding: EdgeInsets.symmetric(vertical: 25.0),
+              width: double.infinity,
+              child: RaisedButton(
+                elevation: 5.0,
+                onPressed: (){
+                  if(boardingPoint != 'Boarding Point'){
+                    FlutterNfcReader.read().then((response) {
+                      Toast.show("Working!!!", context, duration: Toast.LENGTH_LONG, gravity:  Toast.BOTTOM);
+                      print(response.content);
+                    });
                     destination = rnd.nextInt(5).toString();
+                    while(double.parse(destination)==0){
+                      destination = rnd.nextInt(5).toString();
+                    }
+                    destinationPoint='Station '+destination;
                   }
-                  _currentdestination=destination;
-                  destinationPoint='Station '+destination;
-                  DatabaseService(uid: user.uid).updateUserData(
-                      _currentName ?? snapshot.data.name,
-                      _currentbalance ?? snapshot.data.balance,
-                      _currentboarding ?? snapshot.data.boarding,
-                      _currentdestination ?? snapshot.data.destination
-                  );
-                }
-                setState(() {});
-              },
-              onLongPress: () async{
-                if(destinationPoint!='Destination Point') {
-                  boardingPoint = 'Boarding Point';
-                  destinationPoint = 'Destination Point';
-                  setState(() {
+                  setState(() {});
+                },
+                onLongPress: () async{
+                  if(destinationPoint!='Destination Point') {
+                    boardingPoint = 'Boarding Point';
+                    destinationPoint = 'Destination Point';
+                    setState(() {
 
-                    if(boarding!=destination){
-                      if(double.parse(boarding)>double.parse(destination)){
-                        fare = ((double.parse(boarding)-double.parse(destination))*10).toString();
+                      if(boarding!=destination){
+                        if(double.parse(boarding)>double.parse(destination)){
+                          fare = ((double.parse(boarding)-double.parse(destination))*10).toString();
+                        }
+                        else{
+                          fare = ((double.parse(destination)-double.parse(boarding))*10).toString();
+                        }
+                        if(_currentbalance > double.parse(fare) && _currentbalance >0){
+                          _currentbalance = (_currentbalance-double.parse(fare));
+                          DatabaseService(uid: user.uid).updateUserData(
+                              _currentName ?? snapshot.data.name,
+                              _currentbalance ?? snapshot.data.balance,
+                              _currentboarding ?? snapshot.data.boarding,
+                              _currentdestination ?? snapshot.data.destination
+                          );
+                          showDialog(
+                              context: context,
+                              builder: (context)=> AlertDialog(
+                                title: Text('Fare (Station $boarding -> Station $destination) : Rs.$fare'),
+                                actions: <Widget>[
+                                  FlatButton(
+                                    child: Text(
+                                      "Ok",
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                    onPressed: ()=>Navigator.pop(context,true),
+                                  ),
+                                ],
+                              )
+                          );
+                        }
+                        else{
+                          showDialog(
+                              context: context,
+                              builder: (context)=> AlertDialog(
+                                title: Text('Transaction Failed...Insufficient funds'),
+                                actions: <Widget>[
+                                  FlatButton(
+                                    child: Text(
+                                      "Ok",
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                    onPressed: ()=>Navigator.pop(context,true),
+                                  ),
+                                ],
+                              )
+                          );
+                        }
+
                       }
                       else{
-                        fare = ((double.parse(destination)-double.parse(boarding))*10).toString();
-                      }
-                      if(_currentbalance > double.parse(fare) && _currentbalance >0){
-                        _currentbalance = (_currentbalance-double.parse(fare));
-                        DatabaseService(uid: user.uid).updateUserData(
-                            _currentName ?? snapshot.data.name,
-                            _currentbalance ?? snapshot.data.balance,
-                            _currentboarding ?? snapshot.data.boarding,
-                            _currentdestination ?? snapshot.data.destination
-                        );
                         showDialog(
                             context: context,
                             builder: (context)=> AlertDialog(
-                              title: Text('Fare (Station $boarding -> Station $destination) : Rs.$fare'),
+                              title: Text('Fare cant be calculated for same stations'),
                               actions: <Widget>[
                                 FlatButton(
                                   child: Text(
@@ -183,80 +193,40 @@ class _HomeButtonState extends State<HomeButton> {
                             )
                         );
                       }
-                      else{
-                        showDialog(
-                            context: context,
-                            builder: (context)=> AlertDialog(
-                              title: Text('Transaction Failed...Insufficient funds'),
-                              actions: <Widget>[
-                                FlatButton(
-                                  child: Text(
-                                    "Ok",
-                                    style: TextStyle(
-                                      fontSize: 18,
-                                    ),
-                                  ),
-                                  onPressed: ()=>Navigator.pop(context,true),
-                                ),
-                              ],
-                            )
-                        );
-                      }
-
-                    }
-                    else{
-                      showDialog(
-                          context: context,
-                          builder: (context)=> AlertDialog(
-                            title: Text('Fare cant be calculated for same stations'),
-                            actions: <Widget>[
-                              FlatButton(
-                                child: Text(
-                                  "Ok",
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                  ),
-                                ),
-                                onPressed: ()=>Navigator.pop(context,true),
-                              ),
-                            ],
-                          )
-                      );
-                    }
-                  });
-                }
-              },
-              padding: EdgeInsets.all(15.0),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5.0),
-              ),
+                    });
+                  }
+                },
+                padding: EdgeInsets.all(15.0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
 
 //        color: Colors.white,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Icon(Icons.location_on,
-                    color: Colors.green,),
-                  Text(
-                    destinationPoint,
-                    style: TextStyle(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Icon(Icons.location_on,
+                      color: Colors.green,),
+                    Text(
+                      destinationPoint,
+                      style: TextStyle(
 //            color: Color(0xFF478DE0),
-                      letterSpacing: 1.5,
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'OpenSans',
+                        letterSpacing: 1.5,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'OpenSans',
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          );
+            );
 
+          }
+          else{
+            return Loading();
+          }
         }
-        else{
-          return Loading();
-        }
-      }
     );
   }
 
