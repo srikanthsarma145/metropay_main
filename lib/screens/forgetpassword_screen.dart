7dc68ed26cef0de1af4d9d7ef567dc499b66c891
srikanthsarma145 <srikanthsarma145@gmail.com>
2021-01-08 16:41:18 +0530

    commit to 'master'

--- lib/screens/forgetpassword_screen.dart
+++ lib/screens/forgetpassword_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:metropay/utilities/constants.dart';
 import 'package:toast/toast.dart';
+import 'package:metropay/services/auth.dart';
+import 'package:metropay/utilities/loading.dart';
 
 class ForgetScreen extends StatefulWidget {
   @override
@@ -9,42 +11,54 @@ class ForgetScreen extends StatefulWidget {
 }
 
 class _ForgetScreenState extends State<ForgetScreen> {
-  String emailId;
+  final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+  bool loading = false;
+  String eMail = '';
+  String error = '';
 
   Widget _buildemailIdTF() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Text(
-          'Email',
-          style: kLabelStyle,
-        ),
-        SizedBox(height: 10.0),
-        Container(
-          alignment: Alignment.centerLeft,
-          decoration: kBoxDecorationStyle,
-          height: 60.0,
-          child: TextField(
-            keyboardType: TextInputType.emailAddress,
-            onChanged: (text) {
-              emailId = text;
-            },
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'OpenSans',
-            ),
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.only(top: 14.0),
-              prefixIcon: Icon(
-                Icons.alternate_email,
-                color: Colors.white,
-              ),
-              hintText: 'Enter your email',
-              hintStyle: kHintTextStyle,
-            ),
-          ),
-        ),
+        Form(
+            key: _formKey,
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Email',
+                    style: kLabelStyle,
+                  ),
+                  SizedBox(height: 10.0),
+                  Container(
+//          key: _formKey,
+                    alignment: Alignment.centerLeft,
+                    decoration: kBoxDecorationStyle,
+                    height: 60.0,
+                    child: TextFormField(
+                      validator: (val) => val.isEmpty ? 'Enter an email' : null,
+                      onChanged: (val) {
+                        setState(() => eMail = val);
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'OpenSans',
+                      ),
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.only(top: 14.0),
+                        prefixIcon: Icon(
+                          Icons.alternate_email,
+                          color: Colors.white,
+                        ),
+                        hintText: 'Enter your Email',
+                        hintStyle: kHintTextStyle,
+                      ),
+                    ),
+                  ),
+                ]))
       ],
     );
   }
@@ -55,10 +69,23 @@ class _ForgetScreenState extends State<ForgetScreen> {
       width: double.infinity,
       child: RaisedButton(
         elevation: 4.0,
-        onPressed: () {
-          Navigator.pop(context);
-          Toast.show("email sent", context,
-              duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+        onPressed: () async {
+          if (_formKey.currentState.validate()) {
+            setState(() => loading = true);
+            dynamic result = await _auth.sendPasswordResetEmail(eMail);
+            if (result == null) {
+              setState(() {
+                loading = false;
+                error = 'incorrect email or password';
+                Toast.show("Invalid Email", context,
+                    duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+              });
+            } else {
+              Navigator.pop(context);
+              Toast.show("email sent", context,
+                  duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+            }
+          }
         },
         padding: EdgeInsets.all(15.0),
         shape: RoundedRectangleBorder(
@@ -81,73 +108,75 @@ class _ForgetScreenState extends State<ForgetScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: AnnotatedRegion<SystemUiOverlayStyle>(
-        value: SystemUiOverlayStyle.light,
-        child: GestureDetector(
-          onTap: () => FocusScope.of(context).unfocus(),
-          child: Stack(
-            children: <Widget>[
-              Container(
-                height: double.infinity,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                      Color(0xFF73AEF5),
-                      Color(0xFF61A4F1),
-                      Color(0xFF478DE0),
-                      Color(0xFF398AE5),
-                    ],
-                    stops: [0.1, 0.4, 0.7, 0.9],
-                  ),
+    return loading
+        ? Loading()
+        : Scaffold(
+            body: AnnotatedRegion<SystemUiOverlayStyle>(
+              value: SystemUiOverlayStyle.light,
+              child: GestureDetector(
+                onTap: () => FocusScope.of(context).unfocus(),
+                child: Stack(
+                  children: <Widget>[
+                    Container(
+                      height: double.infinity,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Color(0xFF73AEF5),
+                            Color(0xFF61A4F1),
+                            Color(0xFF478DE0),
+                            Color(0xFF398AE5),
+                          ],
+                          stops: [0.1, 0.4, 0.7, 0.9],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: double.infinity,
+                      child: SingleChildScrollView(
+                        physics: AlwaysScrollableScrollPhysics(),
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 40.0,
+                          vertical: 140.0,
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              'MetroPay',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'OpenSans',
+                                fontSize: 30.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 30.0),
+                            _buildemailIdTF(),
+                            SizedBox(height: 20.0),
+                            Text(
+                              'Note: A link to reset your password will be sent to the provided email.',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'OpenSans',
+                                fontSize: 10.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              textAlign: TextAlign.justify,
+                            ),
+                            SizedBox(height: 10.0),
+                            _buildSendEmailBtn(),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
                 ),
               ),
-              Container(
-                height: double.infinity,
-                child: SingleChildScrollView(
-                  physics: AlwaysScrollableScrollPhysics(),
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 40.0,
-                    vertical: 140.0,
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        'MetroPay',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'OpenSans',
-                          fontSize: 30.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 30.0),
-                      _buildemailIdTF(),
-                      SizedBox(height: 20.0),
-                      Text(
-                        'Note: Enter your email in the above text field, the username and password linked to the provided email will be sent to that email.',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'OpenSans',
-                          fontSize: 10.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        textAlign: TextAlign.justify,
-                      ),
-                      SizedBox(height: 10.0),
-                      _buildSendEmailBtn(),
-                    ],
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
